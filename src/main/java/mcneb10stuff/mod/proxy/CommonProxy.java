bd78107f608849500dd28b2c2f90652911acb975
mcneb10 <37050201+mcneb10@users.noreply.github.com>
2018-03-27 10:03:51 -0500

    No More Tile Enities

--- src/main/java/mcneb10stuff/mod/proxy/CommonProxy.java
+++ src/main/java/mcneb10stuff/mod/proxy/CommonProxy.java
@@ -1,7 +1,6 @@
 package mcneb10stuff.mod.proxy;
 
 import mcneb10stuff.mod.MAIN_CLASS;
-import mcneb10stuff.mod.tileentity.TileEntityTV;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class CommonProxy {
@@ -10,9 +9,6 @@ public class CommonProxy {
 		
 	}
 	
-	public void registerTileEnties() {
-		GameRegistry.registerTileEntity(TileEntityTV.class, MAIN_CLASS.MODID + ":tv");
-	}
 
 
 	public void registerModelBakeryVariants() {
