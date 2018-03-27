326dc0edb0b9c614a74103a7a4f16af4e557aa9e
mcneb10 <37050201+mcneb10@users.noreply.github.com>
2018-03-27 10:41:31 -0500

    Add files via upload

---
+++ src/main/java/mcneb10stuff/mod/blocks/IMetaBlockName.java
@@ -0,0 +1,13 @@
+package mcneb10stuff.mod.blocks.item;
+
+import net.minecraft.item.ItemStack;
+
+public interface IMetaBlockName {
+	
+	String getSpecialName(ItemStack stack);
+
+	/**
+	 * @author Mcneb10
+	 */
+	
+}
